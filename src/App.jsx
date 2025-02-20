864ee046834796ddcdee0fa6108be9382ea0e011
aarya moktan <aaryamoktan2055@gmail.com>
2025-02-19 23:12:42 -0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,18 @@ import img2 from "./image/main.jpg"
 import Data from './Api'
 import "./App.css"
 function App() {
+  const [search,setSearch] = useState();
   const [ff,setFF] = useState(Data);
   const menuCAt = [...new Set(Data.map((val)=> val.category))] ;
   const filterItems = (cat)=>
   {
   const newItems = Data.filter((newval)=>newval.category===cat)
   setFF(newItems)
+  }
+  const hello = ()=>
+  {
+    setFF(search)
+    console.log(search)
   }
 console.log(ff)
   return (
@@ -20,7 +26,12 @@ console.log(ff)
             <img src={img} alt="logo" />
           </div>
           <div className='search'>
-            <input type="text" placeholder='Search Food' />
+            <input type="text" placeholder='Search Food' onChange={(e)=>
+              {
+                setSearch(e.target.value)
+              }
+            } />
+        
           </div>
         </div>
         <div className='mid'>
